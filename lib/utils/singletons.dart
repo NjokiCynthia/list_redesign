003c388c4428516cd96a5fa491b2ce03e4e48be6
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-03-12 18:38:26 +0300

    Register the post shoe cubit in singletons file.

--- lib/utils/singletons.dart
+++ lib/utils/singletons.dart
@@ -4,6 +4,11 @@ class Singletons {
   static final _shoeApiService = ShoeApiServiceImpl();
 
   static List<BlocProvider> registerCubits() => [
+    BlocProvider<PostShoeCubit>(
+      create: (context) => PostShoeCubit(
+        shoeApiService: _shoeApiService,
+      ),
+    ),
     BlocProvider<GetItemsCubit>(
       create: (context) => GetItemsCubit(
         shoeApiService: _shoeApiService,
